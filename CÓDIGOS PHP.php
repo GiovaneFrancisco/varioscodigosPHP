d5016f9c980a26b0e9e10eeee17f41c5f0cab641
Giovane Francisco Forlenza <36609868+GiovaneFrancisco@users.noreply.github.com>
2019-01-25 09:29:08 -0200

    Atualização dos códigos

--- CÓDIGOS PHP.php
+++ CÓDIGOS PHP.php
@@ -106,12 +106,18 @@
     date_default_timezone_set(timezone) #Atualiza a timezone default
     strtotime(data) #Transforma uma string de data em segundos
 
- 
-//////////////////////////////////////////////////////////////////////////////////////////
+ 	###########################################################
+    ###########################################################
+    ####Ver como formatar a exibição de data no site do PHP####
+ 	###########################################################
+ 	###########################################################
+
+ //////////////////////////////////////////////////////////////////////////////////////////
 
 	Array
 
-	$lista_arrar = array("item1", "item2", "item3", "item4", "item5");
+	$lista_array = array("item1", "item2", "item3", "item4", "item5");
+	$lista_array = ["item1", "item2", "item3", "item4", "item5"];
 
 	$lista_array[1] = "item1";
 	$lista_array[2] = "item2";
@@ -119,25 +125,94 @@
 	$lista_array[4] = "item4";
 	$lista_array[5] = "item5";
 
-	var_dump($lista_array); #Uso para 
+	$lista_array[] = "item6"; #Dinamicamente insere um item dentro da proxima casa livre
+
+	var_dump($lista_array); #Uso para vizualização do array
 		array(5) {[0]=> string(5)"item1" [1]=> string(5)"item2" .. .. ..}
-	peint_r($lista_array);
+	print_r($lista_array);  #Uso para vizualização do array
 		Array([0]=>item1 [1]=>item2 .. .. ..)
 
+	<pre></pre> #Uso para melhor vizualização do array
+
+
+	$lista_array = array('a' => "item1", '2' => "item2", 'c' => "item3", .. ..);
+	$lista_array['J'] = 'item4'; #Arrays com categorização feita do jeito que precisarmos
+
+
+	in_array("item3', $lista_array); #Retorna true se o item estiver dentro do array
+									 #Retorna false se o item não estiver dentro do array
+
+	array_search("item1", $lista_array); #Retorna o índice do item dentro do array
+										 #Retorna null se o item não existir
+
+
+
+
+	$lista_array['frutas']  = ["Banana", "Maça", "Morango", "Uva"];
+	$lista_array['pessoas'] = ["João", "José", "Maria"];
+		#Array bidimensional, com 2 índices dentro do mesmo nome de array
+
+	echo $lista_array['frutas'][3]; #Faz a recuperação da informação do array 
+
+
+
+	$lista_coisas = ["Frutas" => $lista_frutas, "Pessoas" => $lista_pessoas];
+		#Faz com que o $lista_frutas esteja dentro do índice "Frutas" e o mesmo para pessoas
+		#Desse jeito coloca um array simples dentro de um multidimencional 
+
+	in_array("Maça", $lista_coisas["Frutas"]);
 
 //////////////////////////////////////////////////////////////////////////////////////////
-
-
  
+	is_array($array);  		#Retorna true se for um array, false se não for
+	array_keys($array);		#Retorna as chaves de um array
+		$array = [1=>"a", 8=>"b", 20=>"c"];
+		$chaves_array = array_keys($array);
+
+	sort($array);			#Ordena o array em ordem alfabética, reordenando os índices dos itens
+	asort($array);			#Ordena o array em ordem alfabética, mantendo os índices dos itens
+	count($array);			#Conta a quantidade de itens dentro do array
+	array_merge($array);	#Junta vários arrays dentro de um novo array 
+		$array1 = ["1","2"];
+		$array2 = ["3","4"];
+		$array3 = ["5","6"];
+
+		$novo_array = array_merge($array1, $array2, $array3);
+
+	explode($array);		#Pega uma string e quebra em itens dentro de um array
+		$string = "25/01/2018";
+		$array_retorno = explode("/", $string); #O primeiro item é o que vai definir a divisão dos itens dentoro do array
+
+	implode($array);		#Pega um array e junta em uma string
+		$array = ["a", "b", "x", "y"];
+		$string_retorno implode(",", $array); #O primeiro item define como as irformações vão ser divididas	
+
 //////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-
-
+	is_null($variavel) #True se a variável for null, false se não for
+	empty($variavel)   #True se a variável for vazia, false se não for
  
+
+ 	NUll é exclusivamente NULL, nada mais é 
+ 	Empty pode ser NULL, EMPTY e FALSE
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+	while(condicao){ #Enquanto a condição for verdadeira o bloco de códigos vai ser executado
+		something;
+	}
+ 	
+
+
+ 	do{		#Faz o bloco de código enquanto a condição for verdadeira
+ 		something
+ 	}while(condicao);
+
+
+
+ 	for(variavel; condicao; incremento){
+
+	}
 //////////////////////////////////////////////////////////////////////////////////////////
 
 
